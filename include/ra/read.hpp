7ef1a2e139078534f897623a425c3b5a5d7d8d72
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2022-08-01 14:58:25 -0700

    Scraping the previous approach to READ it becoming too monolithic
    The previous attempt while somewhat functional was making the READ
    function too complicated making it difficult to test, and expand.  The
    new plan is to split read, eval, and print into components. Then
    testing those components individually.

--- include/ra/read.hpp
+++ include/ra/read.hpp
@@ -18,24 +18,26 @@ template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
 // explicit deduction guide (not needed as of C++20)
 template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
 
-enum expr_state {EMPTY, OPEN, READING_SYMBOL,  CLOSED};
 class expression {
 public:
-  using sexpr = std::variant<std::string, expression*>;
-  expression();
+  // TODO replace int with a generalized type that can be used for anything
+  using subexprs = std::vector<expression>;
+  using sexpr = std::variant<subexprs, int>;
+  expression(sexpr s);
   expression(expression& e) = default;
   ~expression();
-  void open ();
-  void close ();
-  bool expressionStarted();
+  expression(const expression& other) = default;
+  sexpr& value();
+  // void open ();
+  // void close ();
+  // bool expressionStarted();
   std::vector<sexpr> expressions() const;
-  void pushSymbol(const symbol& x);
-  void push_subexpression(expression* e);
+  // void pushSymbol(const symbol& x);
+  // void push_subexpression(expression* e);
 
 private:
-  expr_state state = EMPTY;
-  std::vector<sexpr> exprs;
+  sexpr expr;
 };
 std::ostream &operator<<(std::ostream &os, const expression &expr);
-expression* READ(std::string::const_iterator iter, std::string::const_iterator end);
+expression READ(std::string s);
 #endif
