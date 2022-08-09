a587d83c4f225a7e05f06d01a0d314ff644748df
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2022-08-09 15:20:01 -0700

    Added multi line support to the repl

--- app/main.cpp
+++ app/main.cpp
@@ -3,12 +3,29 @@
 #include <cstring>
 #include <iostream>
 
-
 // XXX Temperary hack just so I can get output form an rvalue
-std::string PRINT(expression e){
-  return pr_str(e.value());
+std::string PRINT(expression::sexpr& e){
+  return pr_str(e);
 };
 
+bool getexpression(std::istream& is, std::string& expression_container){
+  int parens = 0;
+  expression_container.clear();
+  std::string accumulator;
+  while (std::getline(is, accumulator)){
+    // Ignore blank lines
+    if (accumulator.empty()) return true;
+    for(auto c : accumulator){
+      if (c == '(') ++parens;
+      if (c == ')') --parens;
+      DBG("there are this many parens left to match " + std::to_string(parens));
+    }
+    expression_container.append(accumulator);
+    std::cout << "reading in" << expression_container << std::endl;
+    if (parens == 0) return true;
+  }
+  return false;
+}
 // the default read-eval-print-loop
 void REPL(bool prompt){
   std::string line;
@@ -19,8 +36,10 @@ void REPL(bool prompt){
   prompter();
   environment env;
   // loop
-  while (std::getline(std::cin, line)){
-    // TODO eval
+  // TODO Support multi line input
+  int parens = 0;
+  while (getexpression(std::cin, line)){
+    if (line.empty()) continue;
     expression e = READ(line);
     std::cout << PRINT(tru_eval(e, env)) << std::endl;
     prompter();
