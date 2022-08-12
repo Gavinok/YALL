e3be598bbc5931be3ed2f89d8846080c3a3c9419
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2022-08-12 10:16:23 -0700

    Added additional missing functions

--- include/ra/eval.hpp
+++ include/ra/eval.hpp
@@ -48,56 +48,181 @@ private:
                 resolve_to<int>(operands->value()) == resolve_to<int>((++operands)->value())
               };
     }},
+    {"eq" , [this](args operands, args_size size) -> sexpr {
+      validate_argument_count(2, size);
 
-        if (operands.size() == 3)
-          return operands[2].value();
+      auto fst = operands, snd = ++operands;
 
-        else
-          if (operands.size() == 2)
-            return boolean{false};
-
-      throw std::runtime_error("if expects at least 2 arguments");
+      // A bit of a hack but this seems to work the most consistantly
+      return sexpr(boolean{ pr_str(fst->value()) == pr_str(snd->value()) });
     }},
-    {"eq" , [](args operands) -> sexpr {
-      // TODO properly compair symbols
-      auto symbol_compair = [](const symbol& a, const symbol& b){ return a[0] == b[0];};
-      return sexpr(
-                   boolean{
-                     std::visit(overloaded{
-                           [](int& a, int& b){ return a == b;},
-                           [](boolean& a, boolean& b){ return a.value == b.value; },
-                           [&symbol_compair](quoted<expression>& a, quoted<expression>& b){
-                             if (symbol_compair(std::get<symbol>((*(a.value)).value()),
-                                                std::get<symbol>((*(b.value)).value())))
-                               return true;
-                             return false;
-                           },
-                           [](auto& a [[gnu::unused]],
-                              auto& b [[gnu::unused]]){
-                              return false;
-                           }
-                          },
-                       operands[0].value(),
-                       operands[1].value())
-                       });
+    {"if" , [](args operands, args_size size) -> sexpr {
+      if(size > 3 ||  size < 2)
+        throw std::runtime_error("Expected 2 or 3 arguments but got " + std::to_string(size));
+      auto condition = std::get<boolean>(operands->value());
+      auto thn = ++operands;
+      auto els = ++operands;
+      return condition.value
+        ? thn->value()
+        : (size == 3) ? els->value() : boolean{false};
     }},
-     {"car" , [](args operands) -> sexpr {
-       auto e = (*(std::get<quoted<expression>>(operands.at(0).value()).value));
-       return quoted<expression>{
-         std::make_shared<expression>( std::get< expression::subexprs >( e.value() ).front().value() )
-       };
-     }},
-     {"cdr" , [](args operands) -> sexpr {
-       auto e = (*(std::get<quoted<expression>>(operands.at(0).value()).value)).value();
-       return quoted<expression>{
-         std::make_shared<expression>(expression::subexprs(std::get<expression::subexprs>(e).begin()+1,
-                                                                                   std::get<expression::subexprs>(e).end()))
-       };
-     }}
-    };
+    {"empty" , [](args operands, args_size size) -> sexpr {
+      if(size < 1)
+        throw std::runtime_error("Expected at least 1 arguments but got " + std::to_string(size));
+      subexprs inner_list = resolve_to<subexprs>(operands->value());
+      return inner_list.empty() ? boolean{true} : boolean{false};
+    }},
+    {"list" , [](args operands, args_size size) -> sexpr {
+      // TODO change this to cons till the end and append an empty list
+      if(size < 1)
+        throw std::runtime_error("Expected at least 1 arguments but got " + std::to_string(size));
+      std::cout << "creating list " << std::endl;
+      auto new_list = std::make_shared<expression>(subexprs{*operands});
+
+      args_size count = 0;
+      for (auto arg = (++operands); count < (size - 1); ++arg,++count)
+        std::get<subexprs>(new_list->value()).push_back(*arg);
+      std::get<subexprs>(new_list->value()).push_back(expression(sexpr{"nil"}));
+
+      return quoted<expression>{ new_list };
+    }},
+    {"append" , [](args operands, args_size size) -> sexpr {
+      validate_argument_count(2, size);
+      subexprs inner_list = resolve_to<subexprs>(operands->value());
+      args other = (++operands);
+      try {
+        subexprs other_list = resolve_to<subexprs>(other->value());
+        inner_list.insert(inner_list.end(), other_list.begin(), other_list.end());
+        DBG(" it's a list '");
+      } catch (...){
+        DBG("Oh nooo")
+        inner_list.push_back(*other);
+      }
+      return quoted<expression>{
+        std::make_shared<expression>( inner_list )
+      };
+    }},
+    {"cons" , [](args operands, args_size size) -> sexpr {
+      validate_argument_count(2, size);
+      subexprs inner_list = subexprs{operands->value()};
+      args other = (++operands);
+      try {
+        subexprs other_list = resolve_to<subexprs>(other->value());
+        inner_list.insert(inner_list.end(), other_list.begin(), other_list.end());
+        DBG(" it's a list '");
+      } catch (...){
+        DBG("Oh nooo")
+        inner_list.push_back(*other);
+      }
+      return quoted<expression>{
+        std::make_shared<expression>( inner_list )
+      };
+    }},
+    {"car" , [](args operands, args_size size) -> sexpr {
+      validate_argument_count(1, size);
+      auto inner_list = resolve_to<subexprs>(operands->value());
+      if (inner_list.size() == 0) throw std::runtime_error("Car called on empty list");
+      return quoted<expression>{
+        std::make_shared<expression>( inner_list.front().value() )
+      };
+    }},
+    {"cdr" , [](args operands, args_size size) -> sexpr {
+      validate_argument_count(1, size);
+      auto inner_list = resolve_to<subexprs>(operands->value());
+      if (inner_list.size() == 0) throw std::runtime_error("Cdr called on empty list");
+      return quoted<expression>{
+        std::make_shared<expression>(subexprs(++(inner_list.begin()), inner_list.end()))
+      };
+    }},
+    {"sleep" , [](args operands, args_size size) -> sexpr {
+      validate_argument_count(1, size);
+
+      auto time = resolve_to<int>(operands->value());
+      sleep(time);
+      return (*operands).value();
+    }},
+    {"print" , [](args operands, args_size size) -> sexpr {
+      validate_argument_count(1, size);
+
+      std::cout << pr_str(operands->value())  << "\n";
+      return (*operands).value();
+    }}
+    // // TODO All of these need to be given the right environemnt
+    // {"map" , [this](args operands, args_size size) -> sexpr {
+    //   validate_argument_count(2, size);
+
+    //   auto func = std::get<expression::lisp_function>(tru_eval(*operands, *this));
+    //   auto e = (*( std::get<quoted<expression>>( (++operands)->value() ).value )).value();
+    //   auto lst = std::get<subexprs>(e);
+
+    //   subexprs accumulator;
+    //   for(auto i : lst){
+    //     auto args = subexprs{i};
+    //     accumulator.push_back(func(args.begin(), args.size()));
+    //   }
+
+    //   return quoted<expression>{
+    //     std::make_shared<expression>(accumulator)
+    //   };
+    // }},
+    // {"filter" , [this](args operands, args_size size) -> sexpr {
+    //   validate_argument_count(2, size);
+
+    //   auto func = std::get<expression::lisp_function>(tru_eval(*operands, *this));
+    //   auto e = (*(std::get<quoted<expression>>((++operands)->value()).value)).value();
+    //   auto lst = std::get<subexprs>(e);
+
+    //   subexprs accumulator;
+    //   for(auto i : lst){
+    //     // XXX expensive
+    //     auto args = subexprs{sexpr(quoted<expression>{std::make_shared<expression>(i)})};
+    //     if(std::get<boolean>(func(args.begin(), args.size()).value()).value)
+    //       accumulator.push_back(i);
+    //   }
+
+    //   return quoted<expression>{
+    //     std::make_shared<expression>(accumulator)
+    //   };
+    // }},
+    // // TODO Handle lists better
+    // {"foldl" , [this](args operands, args_size size) -> sexpr { //  (foldl )
+    //   validate_argument_count(3, size);
+
+    //   auto func = std::get<expression::lisp_function>(tru_eval(*operands, *this));
+    //   auto accumulator = (++operands)->value();
+
+    //   expression::sexpr e =
+    //       (*(std::get<quoted<expression>>((++operands)->value()).value))
+    //           .value();
+    //   auto lst = std::get<subexprs>(e);
+
+    //   for(auto i: lst){
+    //     if (accumulator.index() == i.value().index())
+    //       accumulator =
+    //         std::visit(overloaded{
+    //             [&func](auto & a1, auto& a2) -> sexpr {
+    //               auto args = subexprs{sexpr(a1), sexpr(a2)};
+    //               return func(args.begin(), args.size()).value();
+    //             }},
+    //           accumulator,
+    //           i.value());
+    //   }
+    //   return quoted<expression>{
+    //     std::make_shared<expression>(accumulator)
+    //   };
+    // }},
+    // // Nice to haves
+    // // TODO never said I'd make this avalilable to the user in the proposal but would be a nice to have
+    // // todo can't find info from the outer environment
+    // {"eval" , [this](args operands, args_size size) -> sexpr {
+    //   validate_argument_count(1, size);
+    //   std::cout << "eval this " + pr_str(operands->value()) + "\n" << std::endl;
+    //   expression holder = read_string(pr_str(operands->value()));
+    //   return tru_eval(holder, *this);
+    // }},
+  };
 };
 
 expression::sexpr& eval_subexpressions(expression& expr, environment& env);
-expression::sexpr& tru_eval(expression& expr, environment& env);
 expression::sexpr expand(expression::sexpr s, environment env);
 #endif
